daf75f17b186c8faa5d5916f9ba02410dde717ee
matchy123 <dikomohamed287@gmail.com>
2023-10-08 15:20:11 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber<=400){
+    return "This is on me!";
+  }
+  else if (someNumber>400 && someNumber<=2500)
+  {
+    return 'I will gladly take your thirty bucks.';
+ }
+ else{
+  return "No can do.";
+ }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city=== "NYC" ? "Ok, sounds good." : "No go.";
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous" :
+      return "Thank you so much.";
+    case "not as generous":
+        return "Thank you. ";
+        break;
+    default:
+        return "Bye.";
+        break;
+  }
 }
+
+scuberGreetingForFeet(199)
